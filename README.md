70a7edad272cdb69b68d037ad60103eb83c83d0d
yy <2277144231@qq.com>
2024-10-04 22:34:29 +0800

    实现github授权登录

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 ## 社区-SpringBoot
-第三次commit完成功能
+添加index和bootStrap
 
     添加index页面，
     使用bootStrap完成导航条，
     使用jQuery实现“我”栏下拉功能
 
-## 资料
-SpringBoot插件：https://spring.io/guides
+实现github的授权登录
+    
+    请求用户的的Github标识
+    用户被github重定向回你的站点
+    使用访问令牌访问API,访问方式与视频中不同
+    将github配置从代码提取到属性
 
-前端页面工具bootStrap；https://v3.bootcss.com/getting-started/
+## 资料
+[SpingBoot文档](https://spring.io/guides)  
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)   
+[es](https://elasticsearch.cn/explore)    
+[配置ssh连接Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)    
+[前端页面工具Bootstrap](https://v3.bootcss.com/getting-started/)
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 ## 工具
---
+++ src/main/java/com/example/community/provider/GithubProvider.java
@@ -0,0 +1,52 @@
+package com.example.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component //把当前类初始化到Spring容器上下文
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
+        } catch (IOException e) {
+            return null;
+        }
+
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                //github新版请求方式
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
